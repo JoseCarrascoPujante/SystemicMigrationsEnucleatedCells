4269248c72a230641702293a3d59028b1b392636
JOSE CARRASCO PUJANTE <90905007+JoseCarrascoPujante@users.noreply.github.com>
2023-11-17 20:54:15 +0100

    Update

--- imageJ_emu.m
+++ imageJ_emu.m
@@ -1,5 +1,4 @@
 function imageJ_emu(coordinates)
-    % close all
     fig = uifigure('Position', [0 0 1100 700]);
     ax = uiaxes(fig,'Position', fig.Position);
     hold(ax, "on")
@@ -8,19 +7,17 @@ function imageJ_emu(coordinates)
     f = 1; % N usefulSubfolders
     i = 1; % N experiments in usefulSubfolder
     j = 1; % N tracks in experiment
-
+    
     % Create buttons
-    loadFrames = uibutton('push','Parent',fig,'Text','Load experiment',...
-        'Position',[210 10 100 30],'ButtonPushedFcn', @loadExp);
     Next = uibutton('push','Parent',fig,'Text','Next',...
-        'Position',[110 10 90 30],'ButtonPushedFcn', @nextButton);
+        'Position',[110 5 90 30],'ButtonPushedFcn', @nextButton);
     Prev = uibutton('push','Parent',fig,'Text','Previous',...
-        'Position',[10 10 90 30],'ButtonPushedFcn', @prevButton);
+        'Position',[10 5 90 30],'ButtonPushedFcn', @prevButton);
     displaycurrentframeButton = uibutton('state','Parent',fig,'Position',[1100 715 80 20]);
     
     % Create slider
     slider = uislider(fig, 'Orientation','vertical','Value', 1,...
-                    'Position',[1040 30 1 690]);
+        'Position',[1060 30 1 690], 'ValueChangingFcn', @updateImage);
     
     % Track iteration while loop
       
@@ -28,31 +25,14 @@ function imageJ_emu(coordinates)
         i_n = fieldnames(coordinates.(f_n{f}));
         while i <= size(i_n,1)
             while j <= size(coordinates.(f_n{f}).(i_n{i}).original_x,2)
-    
-                % Plot track and pointer
-    
-                plot(ax,coordinates.(f_n{f}).(i_n{i}).original_x{j}, ...
-                    coordinates.(f_n{f}).(i_n{i}).original_y{j},'Color','r');
-                plot(ax,coordinates.(f_n{f}).(i_n{i}).original_x{j}(end), ...
-                    coordinates.(f_n{f}).(i_n{i}).original_y{j}(end),...
-                    'pentagram','MarkerFaceColor','g','MarkerEdgeColor','g','MarkerSize', 3);
-                p3 = plot(ax,coordinates.(f_n{f}).(i_n{i}).original_x{j}(1), ...
-                    coordinates.(f_n{f}).(i_n{j}).original_y{i}(1),...
-                    'o','MarkerFaceColor','none','MarkerEdgeColor','g','MarkerSize', 8);
-                
-                text(ax,.05,.97,[f_n{f} ' ' i_n{i} ' nº' num2str(j)],...
-                    'Units','normalized','Interpreter','none','Color','m','FontSize',16);
                 
                 % Plot frames
-                if exist('hImage','var') == 0
-                    framestack = loadExp();
-                    hImage=imshow(framestack{1},'Parent',ax);
-                    uistack(hImage,'bottom')
-                    set(slider,'Limits',[1 nframes],'UserData', framestack,...
-                        'MajorTicks', 1:200:nframes,'ValueChangingFcn',...
-                        @(src,event) updateImage(src,event,displaycurrentframeButton,hImage,p3,coordinates.(f_n{f}).(i_n{j}).original_x{j},coordinates.(f_n{f}).(i_n{j}).original_y{j}))
-                end
-                
+                plotData();
+                [framestack, nframes] = loadExp();
+                hImage=imshow(framestack{1},'Parent',ax);
+                uistack(hImage,'bottom')
+                set(slider,'Limits',[1 nframes],'UserData', framestack,...
+                    'MajorTicks', 1:200:nframes)
                 % Wait for user input
                 uiwait(fig)
                                 
@@ -64,12 +44,40 @@ function imageJ_emu(coordinates)
         i = 1;
         f = f + 1;
     end
+    
+    function [pc,pf,pm,pt] = plotData()
+        switch exist("pc","var") && exist("pf","var") && exist("pm","var") && exist("pt","var")
+            case 0
+                % Plot track, pointer & text
+                pc = plot(ax,coordinates.(f_n{f}).(i_n{i}).original_x{j}, ...
+                    coordinates.(f_n{f}).(i_n{i}).original_y{j},'Color','r');
+                
+                pf = plot(ax,coordinates.(f_n{f}).(i_n{i}).original_x{j}(end), ...
+                    coordinates.(f_n{f}).(i_n{i}).original_y{j}(end), ...
+                    'pentagram','MarkerFaceColor','g','MarkerEdgeColor','g','MarkerSize', 3.5);
+                
+                pm = plot(ax,coordinates.(f_n{f}).(i_n{i}).original_x{j}(1), ...
+                    coordinates.(f_n{f}).(i_n{j}).original_y{i}(1), ...
+                    'o','MarkerFaceColor','none','MarkerEdgeColor','g','MarkerSize', 8);
+                
+                pt = text(ax,.05,.97,[f_n{f} ' ' i_n{i} ' nº' num2str(j)], ...
+                    'Units','normalized','Interpreter','none','Color','m','FontSize',16);
+            case 1
+                pc.XData = coordinates.(f_n{f}).(i_n{i}).original_x{j};
+                pc.YData = coordinates.(f_n{f}).(i_n{i}).original_y{j};
+                pf.XData = coordinates.(f_n{f}).(i_n{i}).original_x{j}(end);
+                pf.YData = coordinates.(f_n{f}).(i_n{i}).original_y{j}(end);
+                pm.XData = coordinates.(f_n{f}).(i_n{i}).original_x{j}(1);
+                pm.YData = coordinates.(f_n{f}).(i_n{i}).original_y{j}(1);
+                pt.String = [f_n{f} ' ' i_n{i} ' nº' num2str(j)];           
+        end
+    end
 
     function nextButton(src,~)
         j = j + 1;
         uiresume(fig)
     end
-
+    
     function prevButton(src,~)
         if (j ~= 1)
             j = j - 1;
@@ -88,7 +96,7 @@ function imageJ_emu(coordinates)
         end
         uiresume(fig)
     end
-
+    
     function [framestack, nframes] = loadExp()  
         % Get frame dir and N of frames
         frameDir = uigetdir('C:\Users\JoseC\Desktop\Doctorado\Publicaciones\Papers enucleadas\frames para comprobar tracks sinEst,galv,chem');
@@ -103,14 +111,13 @@ function imageJ_emu(coordinates)
         toc
     end
     
-    function updateImage(src,event,dispframeButton,hImage,p3,coordsX,coordsY)
+    function updateImage(src,event,dispframeButton,hImage,pm,coordsX,coordsY)
         idx = round(event.Value) ;
         hImage.CData = src.UserData{idx}(:,:);
-        p3.XData = coordsX(idx);
-        p3.YData = coordsY(idx);
+        pm.XData = coordsX(idx);
+        pm.YData = coordsY(idx);
         if dispframeButton.Value == 1
             dispframeButton.Text = num2str(idx);
         end
     end
-
 end
